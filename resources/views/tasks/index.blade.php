f5d7541d4f9b01a0b320bc9bafa0e63c2216e5f7
sudip6164 <sudippradhanadgj@gmail.com>
2026-02-13 13:34:05 +0545

    refactor: remove 'Add New Task' link from tasks index page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,5 @@
 <x-layout title="Tasks">
     <h1>My Tasks</h1>
-    <a href="{{ route('tasksCreatePage') }}">Add New Task</a><br><br>
 
     @foreach($tasks as $task)
         <b>{{ $task->title }}</b><br>
